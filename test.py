f104a332bc399866dca573b90086f2a205aec628
AndreyPankov89 <apan89@mail.ru>
2021-07-25 18:15:37 +0500

    Add files via upload

--- test.py
+++ test.py
@@ -1,239 +1,265 @@
+# -*- coding: utf-8 -*-
+import random
 import os
 import jsonpickle
+
 jsonpickle.set_encoder_options('json', indent=4, separators=(',', ': '), ensure_ascii=False)
-
-
-class School_attribute:
-    def __init__(self,  attribute, data):
-        self.attribute = attribute
-        self.data = data
-
-
-class Check:
-    def check_empty_space(string):
-        string = str(string)
-        if len(string) == 0:
-            print('Данный пункт не может быть пустым, введите корректное данные')
-            return True
-        else:
-            if string.isspace():
-                print('Данный пункт не может состоять из пробелов, введите корректное данные')
-                return True
-            else:
-                return False
-
-    def check_alpha(string):
-        string = str(string)
-        replace_string = string.replace(' ','-')
-        split_string = replace_string.split('-')
-        # можно лучше: Добавить тримминг пробелов, чтобы строка "Иванов " верифицировалась как корректная
-        for element in range(len(split_string)):
-            if not split_string[element].isalpha():
-                print('Данный пункт должен состоять из букв, введите корректные данные')
-                return False
-        return True
-
-
-class School_attribute_Storage:
-    def __init__(self):
-        self.file_name = 'school.json'
-
-    def overwriting():
-        while True:
-            name_school = input('Введите название школы: ')
-            if Check.check_empty_space(name_school):                
-                continue
-            else:
-                break
-
-        while True:
-            adress_school = input('Введите адрес школы: ')
-            if Check.check_empty_space(adress_school):
-                continue
-            else:
-                break
-
-        school = [School_attribute('Название школы:', name_school), School_attribute('Адрес школы:', adress_school)]
-        data = jsonpickle.encode(school)
-        FileProvider.update_file('school.json', data)
-
-    def get_all():
-        code_school = FileProvider.get('school.json')
-        school = jsonpickle.decode(code_school)
-        for attribute in range(len(school)):
-            print(school[attribute].attribute, school[attribute].data)
-
-        #Можно лучше: Количество студентов хранить в поле класса, чтобы лишний раз не дергать файл.
-        if not FileProvider.exist('students.json'):
-            print('Количество учеников: 0')
-        else:            
-            code_students = FileProvider.get('students.json')
-            students = jsonpickle.decode(code_students)
-            number_of_students = len(students)
-            print('Количество учеников:', number_of_students)
-
+file_name = 'schools.json'
 
 class FileProvider:
-    def exist(path):
-        return os.path.exists(path)
-
-    def get(path):
+    def get(self, path):
         file = open(path, 'r')
         data = file.read()
         file.close()
         return data
 
-    def update_file(path, data):        
+    def append(self, path, data):
+        file = open(path, 'a')
+        data = file.write(data)
+        file.close()
+
+    def writelines(self, path, data):
         file = open(path, 'w')
-        file.write(data)
+        data = file.write(data)
+        file.close()
+
+    def exists(self, path):
+        return os.path.exists(path)
+
+    def clear(self, path):
+        file = open(path, 'w')
+        file.write("")
         file.close()
 
 
+
+
+class School:
+    def __init__(self, name, adress):
+        self.name = name
+        self.adress = adress
+        self.students = []
+
+# GET говорит нам о том, что функция доолжна возвращать студентов. не печатать, а именно возвращать. Печать это отдельная задача
+    def get_all_students(self, select_item):
+        name = 'Имя ученика'
+        age = 'Возраст'
+        class_number = 'Номер класса'
+        print(f'{name:15}{age:10}{class_number:10}')
+
+        students = select_item
+        for player in students:
+            print(f'{player.students.name:15}{player.students.age:10}{player.students.class_number:10}')
+
+    
+
+
+class Storage:
+    def __init__(self, path):
+        self.file_path = path
+
+    def get_all(self):
+        if not file_provider.exists(self.file_path):
+            schools = []
+            self.safe_data(schools)
+
+        data = file_provider.get(self.file_path)
+        schools = jsonpickle.decode(data)
+        return schools
+
+    def add_data(self, school):
+        schools = self.get_all()
+        schools.append(school)
+        self.safe_data(schools)
+
+    def safe_data(self, schools):
+        json_data = jsonpickle.encode(schools)
+        file_provider.writelines(self.file_path, json_data)
+
+    def edit_data(self, schools_list):
+        schools = self.get_all()
+        schools = schools_list
+        self.safe_data(schools)
+
 class Student:
-    def __init__(self, student_name, age, class_number):
-        self.student_name = student_name
+    def __init__(self, name, age, class_number):
+        self.name = name
         self.age = age
         self.class_number = class_number
 
-    def add_student():
+
+file_provider = FileProvider()
+storage = Storage(file_name)
+
+def print_schools():
+    school_list = storage.get_all()
+    for schools in school_list:
+        print(f'{schools.name:15}{schools.adress:15}')
+
+
+
+while True:
+    print('Выберите что вы хотите сделать:')
+    print("1. Добавить данные о новой школе:")
+    print("2. Получение полной информации о школе:")
+    print("3. Изменение информации о школе")
+    print("4. Просмотр учеников школы в виде таблицы")
+    print("5. Добавление нового ученика школы")
+    print("6. Удаление имеющегося ученика школы")
+    print("0. выйти из программы")
+    cmd = input("Выберите пункт: ")
+
+#Общее замечание, то что в теле if, лучше вынести в отдельныее функции, а тут вызывать их, чтобы код  был более читаемым и чистым
+
+    if cmd == "1":
+        print('Давайте введем информацио о школе')
+        name = input('Введите название школы \n')
+        adress = input('Введите адресс школы \n')
+        #Как выглядет проверка в общем случае?
+        '''
         while True:
-            name = input('Введите ФИО ученика: ').title()
-            if Check.check_empty_space(name):
-                continue
-            if not Check.check_alpha(name):
-                continue
+            name = input()
+            if name == '':
+                print('error')
             else:
                 break
-
+        '''
+        if name == '' or adress == '':
+            print('Вы ввели пустую строку')
+            continue
+        school = School(name, adress)
+        storage.add_data(school)
+    elif cmd == "2":
+        school_list = storage.get_all()
+# Вот тут ведь правильно организовал проверку
         while True:
-            age = input('Введите возраст ученика: ')
-            if not age.isdigit():
-                print('Данный пункт должен состоять из числа, введите корректные данные')
+            print('Выберите школу о которой хотите получить информацию:')
+            for i in range(len(school_list)):
+                print(f'{i + 1}.{school_list[i].name:15}')
+            user_input = input('\n')
+# я бы еще проверил, что такое число школ есть. Например школ всего 3, а ввели число 7
+            if not user_input.isdigit():
+                print('Вы ввели не число')
                 continue
-            #Примечание. Не ошибка, но лучше не переприсваивать значение переменной другого типа. Сначала в age у нас строка, потом число. лучше использовать разные переменные
-            age = int(age)
-            if age < 5 or age >= 100:
-                print('Возраст ученика не может быть меньше 5 и больше 99 лет, введите корректные данные')
-                continue
-            else:
-                break
-        age = str(age)
-
+            index = int(user_input) - 1
+            select_item = school_list[index]
+            print(f'{select_item.name:15}{select_item.adress:15}')
+            break
+    elif cmd == "3":
+        school_list = storage.get_all()
+        print('Выберите школу которую хотите изменить')
+        for i in range(len(school_list)):
+            print(f'{i + 1}.{school_list[i].name:15}{school_list[i].adress:15}')
+        user_input = input('\n')
+#а тут опять не правильно
+        if not user_input.isdigit():
+            print('Вы ввели не число')
+            continue
+        index = int(user_input) - 1
+        select_item = school_list[index]
+        select_item.name = input('Введите новое название \n')
+        if select_item.name == '':
+            print('Вы ввели пустую строку')
+            continue
+        select_item.adress = input('Введите новый адресс \n')
+        if select_item.adress == '':
+            print('Вы ввели пустую строку')
+            continue
+#  не понял логику, что ты тут нагородил.
+#Тебе надо у scool_list[index] переприсвоить имя и адрес. Причем я бы сделал так. Если имя или адрес не введен (пустая строка), то не переприсваивать
+        school_list[index] = select_item
+        for i in range(len(school_list)):
+            storage.edit_data(school_list)
+    elif cmd == "4":
+        schools = storage.get_all()
+    #У тебя вывод списка школ и запрос номера повторяется везде. Можно вынести в отдельную функцию, которая принимает schools и возвращает номер, который выбран
+        print('Выберите школу в которой хотите посмотреть учеников')
+        for i in range(len(schools)):
+            print(f'{i + 1}.{schools[i].name:15}{schools[i].adress:15}')
+    #Проверь что будет если введешь не число
+        user_input = input('\n')
         while True:
-            class_number = input('Введите номер класса в котором обучается ученик: ')
-            # Поправить!!! Класс может быть с буквой. Например 5а, 6г и т.п. ну и классов максимум 12
-            if not class_number.isdigit():
-                print('Данный пункт должен состоять из числа, введите корректные данные')
+            if not user_input.isdigit():
+                print('Вы ввели не число')
                 continue
-            class_number = int(class_number)
-            if class_number < 1 or class_number > 20:
-                print('Номер класса не может быть менее 1 и более 20, введите корректные данные')
-                continue
-            else:
-                break
-        class_number = str(class_number)
+            break
+        index = int(user_input) - 1
+        select_item = schools[index]
+        name = 'Имя ученика'
+        age = 'Возраст'
+        class_number = 'Номер класса'
+        print(f'{name:15}{age:10}{class_number:10}')
 
-        new_student = Student(name, age, class_number)
-
-        #Можно лучше: не помешала бы оптимизация. вывод в файл можно вынести за if
-        if not FileProvider.exist('students.json'):
-            new_student = [new_student]
-            code_new_student = jsonpickle.encode(new_student)
-            FileProvider.update_file('students.json', code_new_student)
-        else:
-            code_students = FileProvider.get('students.json')
-            students = jsonpickle.decode(code_students)
-            students.append(new_student)
-            new_code_students = jsonpickle.encode(students)
-            FileProvider.update_file('students.json', new_code_students)
-
-    def del_student():
-        if not FileProvider.exist('students.json'):
-            print('Нет доступных учеников для удаления!')
-        else:
-            code_students = FileProvider.get('students.json')
-            students = jsonpickle.decode(code_students)
-            if len(students) == 0:
-                print('Нет доступных учеников для удаления!')
-            else:
-                for student in range(len(students)):
-                    print(student+1, students[student].student_name)
-
-                while True:
-                    number_student = input('Введите номер ученика для удаления: ')
-                    if not number_student.isdigit():
-                        print('Данный пункт должен состоять из числа, введите корректные данные')
-                        continue
-                    else:
-                        number_student = int(number_student)
-                        if number_student < 1 or number_student > len(students):
-                            print('Введите номер ученика от 1 до', len(students))
-                        else:
-                            break
-
-                students.pop(number_student-1)
-                new_dell_code_students = jsonpickle.encode(students)
-                FileProvider.update_file('students.json', new_dell_code_students)
-
-    def show_students():
-        if not FileProvider.exist('students.json'):
-            print('В школе нет учеников!')
-        else:
-            code_students = FileProvider.get('students.json')
-            students = jsonpickle.decode(code_students)
-            if len(students) == 0:
-                print('В школе нет учеников!')
-            else:                
-
-#Если нужно выровнять по центру, можно написать типа такого: f'{value:^30}. Тогда под вывод выделится 30 знакомест, а value встанет по середине
-
-                print(f'{"НОМЕР":30}{"ИМЯ":40}{"ВОЗРАСТ":30}{"НОМЕР КЛАССА":30}')
-                for student in students:
-                    print(f'{str(students.index(student)+1):22}{student.student_name:52}{student.age:33}{student.class_number:10}')
-
-
-class Menu:
-    def main_menu(self):
+        students = select_item.students
+        for i in range(len(students)):
+            print(f'{i + 1}.{students[i].name:15}{students[i].age:10}{students[i].class_number:10}')
+    elif cmd == "5":
+    
+#Тоже проверка не прваильная. См пример выше.
+        student_name = input('Введите ФИО ученика \n')
+        if student_name == '':
+            print('Вы ввели пустую строку')
+            continue
+        student_age = input('Введите возраст ученика \n')
+        if student_age == '' or not student_age.isdigit():
+            print('Вы ввели некоректные данные')
+            continue
+        class_number = input('Введите номер класса \n')
+        if class_number == '' or not class_number.isdigit():
+            print('Вы ввели некоректные данные')
+            continue
+        new_student = Student(student_name, student_age, class_number)
+        schools = storage.get_all()
+        print('Выберите школу в которую хотите добавить ученика')
+        for i in range(len(schools)):
+            print(f'{i + 1}.{schools[i].name:15}{schools[i].adress:15}')
+        user_input = input('\n')
         while True:
-            print()
-            main_menu = ['Получение полной информации о школе', 'Изменение информации о школе', 'Просмотр учеников школы', 'Добавление нового ученика школы', 'Удаление имеющегося ученика школы', 'Выход из программы']
-            for element in range(len(main_menu)):
-                print(element+1, main_menu[element])
+            if not user_input.isdigit():
+                print('Вы ввели не число')
+                continue
+            break
+        index = int(user_input) - 1
+        select_item = schools[index]
+        select_item.students.append(new_student)
+        schools[index] = select_item
+        for i in range(len(schools)):
+            storage.edit_data(schools)
+    elif cmd == "6":
+        schools = storage.get_all()
+        print('Выберите школу в которой хотите удалить ученика')
+        for i in range(len(schools)):
+            print(f'{i + 1}.{schools[i].name:15}{schools[i].adress:15}')
+        user_input = input('\n')
+        while True:
+            if not user_input.isdigit():
+                print('Вы ввели не число')
+                continue
+            break
+        index = int(user_input) - 1
+        select_item = schools[index]
+        name = 'Имя ученика'
+        age = 'Возраст'
+        class_number = 'Номер класса'
+        print(f'{name:15}{age:10}{class_number:10}')
+        students = select_item.students
+        for i in range(len(students)):
+            print(f'{i + 1}.{students[i].name:15}{students[i].age:10}{students[i].class_number:10}')
+        print('Выберите ученика')
 
-            print()
-            while True:
-                command = input('Введите номер команды: ')
-                if not command.isdigit():
-                    print('Данный пункт должен состоять из числа, введите корректные данные')
-                    continue
-                else:
-                    command = int(command)
-                    if command < 1 or command > len(main_menu):
-                        print('Введите номер команды от 1 до', len(main_menu))
-                        continue
-                    else:
-                        break
-
-            print()
-            if command == 1:
-                School_attribute_Storage.get_all()
-            if command == 2:
-                School_attribute_Storage.overwriting()
-            if command == 3:
-                Student.show_students()
-            if command == 4:
-                Student.add_student()
-            if command == 5:
-                Student.del_student()
-            if command == 6:
-                print('До свидания!')
-                break
-
-
-main_menu = Menu()
-print('Здравствуйте')
-
-if not FileProvider.exist('school.json'):
-    School_attribute_Storage.overwriting()
-
-main_menu.main_menu()
+    #Опять что будет если введешь не число?
+        user_select = input('\n')
+        while True:
+            if not user_select.isdigit():
+                print('Вы ввели не число')
+                continue
+            break
+        select = int(user_select) - 1
+        students.pop(select)
+        for i in range(len(schools)):
+            storage.edit_data(schools)
+    elif cmd == "0":
+        break
+    else:
+        print("Вы ввели не правильное значение")
+    break
